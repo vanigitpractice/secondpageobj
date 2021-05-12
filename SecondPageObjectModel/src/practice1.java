3d747f6975a7e1eed3585be8d50dbf34a7c1165e
vanigitpractice <83716737+vanigitpractice@users.noreply.github.com>
2021-05-12 14:56:27 -0400

    Update practice1.java
    test on changes

--- SecondPageObjectModel/src/practice1.java
+++ SecondPageObjectModel/src/practice1.java
@@ -3,7 +3,7 @@ public class practice1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("practice on git");
+         system.out.println("my changes");
 	}
 
 }
