01e289ca85bf90ada799f9f9b14e6e86d645cc55
practicet20@gmail.com <practicet20@gmail.com>
2021-05-12 14:47:09 -0400

    removed extra message

--- SecondPageObjectModel/src/practice1.java
+++ SecondPageObjectModel/src/practice1.java
@@ -4,7 +4,6 @@ public class practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("practice on git");
-         System.out.println("brach practice");
 	}
 
 }
