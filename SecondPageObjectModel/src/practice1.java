a7142592e95e48faf50226dcee754743017fc5dd
practicet20@gmail.com <practicet20@gmail.com>
2021-05-12 14:58:23 -0400

    new changes

--- SecondPageObjectModel/src/practice1.java
+++ SecondPageObjectModel/src/practice1.java
@@ -4,6 +4,7 @@ public class practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("practice on git");
+         System.out.println("my new changes");
 	}
 
 }
